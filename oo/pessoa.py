bbb17d6f64aee07226f0747c3536e0ebc0f97e02
Nathan Lubawski <nathanlubawski1@gmail.com>
2021-11-17 20:29:22 -0300

    Criados atributos de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
+
     def cumprimentar(self):
         return f'ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lubawski')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = "Nathan"
+    print(p.nome)
 
