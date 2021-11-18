615c3d3f624f975406936e9674f895c8829178da
Nathan Lubawski <nathanlubawski1@gmail.com>
2021-11-18 08:56:16 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+
+    olhos = 2 #atributo de classe ou default, assim todos objetos usam ele e alocamos menos memoria
+
     def __init__(self, *filhos, nome=None, idade=None ):
         self.nome = nome
         self.idade = idade
@@ -21,7 +24,14 @@ if __name__ == '__main__':
     nathan.sobrenome = 'teste' #criacao de atributo dinamico
     print(nathan.sobrenome)
     del lubawski.filhos # deletando atributo dinamicamente
+    lubawski.olhos = 1
     print(nathan.__dict__)   #verificar todos atributos de instancia
     print(lubawski.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos) #atributo de classe voce acessa pela classe ou pelos objetos
+    print(nathan.olhos)
+    print(lubawski.olhos)
+    print(id(Pessoa.olhos), id(nathan.olhos), id(lubawski.olhos))
+
 
 
